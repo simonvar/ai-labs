b166747d081820eb02d61b8a218937b4c68ef1e1
PaladiRka <milakins@bk.ru>
2019-10-07 23:49:12 +0300

    Implementation for HeuristicGamer and test for him add.

--- src/main/kotlin/gamer/HeuristicGamer.kt
+++ src/main/kotlin/gamer/HeuristicGamer.kt
@@ -2,6 +2,9 @@ package gamer
 
 import Desc
 import Heuristic
+import Node
+import fold
+import moveAll
 
 class HeuristicGamer(
     private val heuristic: Heuristic,
@@ -9,7 +12,29 @@ class HeuristicGamer(
 ) : (Desc) -> Desc {
 
     override fun invoke(desc: Desc): Desc {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val head = Node(desc)
+        return next(head, head)?.value ?: throw IllegalArgumentException()
+    }
+
+    private fun next(head: Node<Desc>, prevNode: Node<Desc>): Node<Desc>? {
+        visitor(prevNode.value)
+
+        val descs = prevNode.value.moveAll()
+        val newDescs = head.fold(descs) { acc, item ->
+            //            acc.filter { heuristic(it) <= heuristic(item) }
+            acc.filter { it != item }
+        }
+        val newDesc = head.fold(newDescs) { acc, item ->
+            //            acc.filter { heuristic(it) <= heuristic(item) }
+            acc.filter { heuristic(it) <= heuristic(item) }
+        }
+        prevNode.nextNodes = newDesc.map { Node(it) }
+        prevNode.nextNodes.forEach {
+            val next = next(head, it)
+            if (next != null) return next
+        }
+
+        return null
     }
 
 }
