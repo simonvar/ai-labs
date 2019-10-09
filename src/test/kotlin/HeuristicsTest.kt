63a0305f294a2c6a34fb7b17b4e4c88b7dc551f4
PaladiRka <milakins@bk.ru>
2019-10-09 23:48:09 +0300

    Добавлен тест, проверяющий состояние зеркальное к правильному.

--- src/test/kotlin/HeuristicsTest.kt
+++ src/test/kotlin/HeuristicsTest.kt
@@ -69,6 +69,24 @@ class HeuristicsTest {
         Assert.assertEquals(0F, h2(actualDesc))
     }
 
+    @Test
+    fun `h2 test false finish`() {
+        val cells = listOf(
+            1.toCell(), 2.toCell(), 3.toCell(),
+            4.toCell(), Cell.EmptyCell, 7.toCell(),
+            6.toCell(), 5.toCell(), 8.toCell()
+        )
 
+        val finishCells = listOf(
+            1.toCell(), 2.toCell(), 3.toCell(),
+            4.toCell(), Cell.EmptyCell, 5.toCell(),
+            6.toCell(), 7.toCell(), 8.toCell()
+        )
 
+        val desc = Desc(cells)
+        val finishDesc = Desc(finishCells)
+        val hHeuristic = H2(finishDesc)
+
+        Assert.assertNotEquals(0f, hHeuristic(desc))
+    }
 }
