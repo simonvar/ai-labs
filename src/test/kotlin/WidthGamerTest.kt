5aec7515672ef749a7fae20069fe8f3c18da95d1
PaladiRka <milakins@bk.ru>
2019-10-09 23:59:58 +0300

    Width Gamer remove graph running and change to list.

---
+++ src/test/kotlin/WidthGamerTest.kt
@@ -0,0 +1,37 @@
+import org.junit.Assert
+import org.junit.Test
+import gamer.WidthGamer
+
+class WidthGamerTest {
+    @Test
+    fun `test correct solve`() {
+        val cells = listOf(
+            4.toCell(), 8.toCell(), 1.toCell(),
+            Cell.EmptyCell,3.toCell(), 6.toCell(),
+            2.toCell(), 7.toCell(), 5.toCell()
+        )
+
+        val finishCells = listOf(
+            1.toCell(), 2.toCell(), 3.toCell(),
+            8.toCell(), Cell.EmptyCell, 4.toCell(),
+            7.toCell(), 6.toCell(), 5.toCell()
+        )
+//        val finishCells = listOf(
+//            7.toCell(), 4.toCell(), 2.toCell(),
+//            Cell.EmptyCell, 5.toCell(), 8.toCell(),
+//            3.toCell(), 6.toCell(),  1.toCell()
+//        )
+
+        val desc = Desc(cells)
+        val finishDesc = Desc(finishCells)
+        var i = 0
+        val resultGame = WidthGamer(finishDesc) {
+//            println(i)
+//            println(it)
+            i++
+        }
+        Assert.assertEquals(finishDesc, resultGame.invoke(desc))
+    }
+
+
+}
